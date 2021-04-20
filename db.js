0f4aad5dd2565a9978a55ad08a6649abcce338aa
Franklin <kaifenzheng@gmail.com>
2021-04-19 22:01:55 -0400

    probably better to make it a secret

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
+require("dotenv").config()
 const Pool = require("pg").Pool
 
 const pool = new Pool({
-    user: "postgres",
-    password: "mynamenigel",
-    database: "crocodiloDB",
-    host: "crocodilo-db.ctakohz82bfj.us-east-2.rds.amazonaws.com",
-    port: 5432
+    user: process.env.AWS_POSTGRES_USERNAME,
+    password: process.env.AWS_POSTGRES_USERNAME,
+    database: process.env.AWS_POSTGRES_DB_NAME,
+    host: process.env.AWS_POSTGRES_HOST,
+    port: process.env.AWS_POSTGRES_PORT
 })
 
 module.exports = pool
