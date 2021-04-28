375085fd57c6c50de07eb0992bd5b1760f711169
Harrison Chen <hh6chen@edu.uwaterloo.ca>
2021-04-28 10:38:43 -0400

    Feature/harrison (#9)
    * template for express app
    
    * babel + nodemon setup
    
    * linting rules

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const indexRouter = express.Router();
+
+indexRouter.get('/', (req, res) =>
+  res.status(200).json({ message: 'Welcome to Express API template' })
+);
+
+export default indexRouter;
