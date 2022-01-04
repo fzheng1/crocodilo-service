db3beb5fa046db2e7417bc5307d20b1edb2ab33f
Harrison <harrisonchen1218@gmail.com>
2022-01-04 12:45:36 -0500

    flask > express

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const indexRouter = express.Router();
-
-indexRouter.get('/', (req, res) =>
-  res.status(200).json({ message: 'Welcome to Express API template' })
-);
-
-export default indexRouter;
